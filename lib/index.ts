b57e304bc0edca18c133caf806037d4b6040eeed
Juan Cruz Viotti <jv@jviotti.com>
2022-05-23 18:25:13 -0400

    Prototype index
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,33 @@
 import WalkerJSONSchema2019_19 from './walkers/jsonschema-2019-09'
 
+import {
+  JSONValue
+} from './json'
+
+import {
+  Rule
+} from './rules/rule'
+
+import {
+  Walker
+} from './walkers/walker'
+
+import {
+  SchemaType
+} from './schema'
+
+import {
+  transformSchema
+} from './transformer'
+
 export const walkers = {
   JSONSchema2019_19: WalkerJSONSchema2019_19
+}
+
+export const alterSchema = (value: JSONValue, ruleset: Rule[], walker: Walker = WalkerJSONSchema2019_19): JSONValue => {
+  walker(value, (input: JSONValue, _type: SchemaType): void => {
+    transformSchema(input, ruleset)
+  })
+
+  return value
 }
