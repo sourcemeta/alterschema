4f452318f4023f14fcc07d2e2732fabd201c3474
Juan Cruz Viotti <jv@jviotti.com>
2022-05-23 17:51:31 -0400

    Design transformer
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ lib/transformer.ts
@@ -0,0 +1,41 @@
+import {
+  Rule
+} from './rules/rule'
+
+import {
+  JSONValue
+} from './json'
+
+import {
+  strict as assert
+} from 'assert'
+
+import {
+  cloneDeep
+} from 'lodash'
+
+export const transformSchema = (value: JSONValue, ruleset: Rule[]): JSONValue => {
+  const result = ruleset.reduce((accumulator, rule) => {
+    // Guard against rules accidentally modifying the input document
+    // Its easier than it sounds to make such mistake and JavaScript
+    // does not provide true "const" semantics.
+
+    if (!rule.condition(cloneDeep(accumulator))) {
+      return accumulator
+    }
+
+    const output = rule.transform(cloneDeep(accumulator))
+    assert(!rule.condition(cloneDeep(output)), 'Rule condition must not match after transform')
+    return output
+  }, value)
+
+  // If at least one rule still matches the result,
+  // then we recurse, until no rule matches again.
+  if (ruleset.some((rule) => {
+    return rule.condition(result)
+  })) {
+    return transformSchema(result, ruleset)
+  }
+
+  return result
+}
