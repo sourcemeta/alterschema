54cd3dd87ed3e3468779cd1520491238c7fdfea8
Juan Cruz Viotti <jv@jviotti.com>
2022-05-23 17:36:25 -0400

    Delete dist
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- dist/lib/jsonschema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usesVocabulary = void 0;
-var usesVocabulary = function (_value, _spec, _vocabulary) {
-    return true;
-};
-exports.usesVocabulary = usesVocabulary;
