65fff089f8a9a311f4a76a6cd9f2e161e70e67e5
Juan Cruz Viotti <jv@jviotti.com>
2022-05-23 20:34:20 -0400

    Add handle function
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- lib/walkers/jsonschema-2019-09.ts
+++ lib/walkers/jsonschema-2019-09.ts
@@ -15,6 +15,19 @@ import {
   usesVocabulary
 } from '../jsonschema'
 
+const handle = (walker: Walker, root: JSONValue, path: string[], vocabulary: string, keyword: string): WalkerElement[] => {
+  const value = path.length === 0 ? root : get(root, path)
+  if (typeof value !== 'object' || Array.isArray(value) || value === null) {
+    return []
+  }
+
+  if (usesVocabulary(root, value, vocabulary) && keyword in value) {
+    return walker(root, path.concat([ keyword ]))
+  }
+
+  return []
+}
+
 const walk: Walker = (root: JSONValue, path: string[]): WalkerElement[] => {
   const result: WalkerElement[] = []
   // The top-level is always a schema
@@ -52,21 +65,10 @@ const walk: Walker = (root: JSONValue, path: string[]): WalkerElement[] => {
       }
     }
 
-    if ('not' in value) {
-      result.push(...walk(root, path.concat([ 'not' ])))
-    }
-
-    if ('if' in value) {
-      result.push(...walk(root, path.concat([ 'if' ])))
-    }
-
-    if ('then' in value) {
-      result.push(...walk(root, path.concat([ 'then' ])))
-    }
-
-    if ('else' in value) {
-      result.push(...walk(root, path.concat([ 'else' ])))
-    }
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'not'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'if'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'then'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'else'))
 
     if ('dependentSchemas' in value) {
       for (const key of Object.keys(value.dependentSchemas)) {
@@ -84,17 +86,9 @@ const walk: Walker = (root: JSONValue, path: string[]): WalkerElement[] => {
       }
     }
 
-    if ('additionalItems' in value) {
-      result.push(...walk(root, path.concat([ 'additionalItems' ])))
-    }
-
-    if ('unevaluatedItems' in value) {
-      result.push(...walk(root, path.concat([ 'unevaluatedItems' ])))
-    }
-
-    if ('contains' in value) {
-      result.push(...walk(root, path.concat([ 'contains' ])))
-    }
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'additionalItems'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'unevaluatedItems'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'contains'))
 
     if ('properties' in value) {
       for (const key of Object.keys(value.properties)) {
@@ -108,25 +102,12 @@ const walk: Walker = (root: JSONValue, path: string[]): WalkerElement[] => {
       }
     }
 
-    if ('additionalProperties' in value) {
-      result.push(...walk(root, path.concat([ 'additionalProperties' ])))
-    }
-
-    if ('unevaluatedProperties' in value) {
-      result.push(...walk(root, path.concat([ 'unevaluatedProperties' ])))
-    }
-
-    if ('propertyNames' in value) {
-      result.push(...walk(root, path.concat([ 'propertyNames' ])))
-    }
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'additionalProperties'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'unevaluatedProperties'))
+    result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/applicator', 'propertyNames'))
   }
 
-  if (usesVocabulary(root, value, 'https://json-schema.org/draft/2019-09/vocab/content')) {
-    if ('contentSchema' in value) {
-      result.push(...walk(root, path.concat([ 'contentSchema' ])))
-    }
-  }
-
+  result.push(...handle(walk, root, path, 'https://json-schema.org/draft/2019-09/vocab/content', 'contentSchema'))
   return result
 }
 
