dc5fd414de02c99c3b66435a5d6808996e6536fd
Juan Cruz Viotti <jv@jviotti.com>
2022-05-24 19:55:18 -0400

    Simplify runner
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/walkers.spec.js
+++ test/walkers.spec.js
@@ -1,17 +1,10 @@
 const tap = require('tap')
-const path = require('path')
-const fs = require('fs')
 
-for (const walker of [
-  {
-    name: 'jsonschema-2019-09.json',
-    fn: require('../lib/walkers/jsonschema-2019-09')
-  }
-]) {
-  const testCases = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'walkers', walker.name), 'utf8'))
-  for (const testCase of testCases) {
+for (const walker of [ 'jsonschema-2019-09' ]) {
+  for (const testCase of require(`./walkers/${walker}.json`)) {
+    const fn = require(`../lib/walkers/${walker}`)
     tap.test(`${walker.name}: ${testCase.name}`, (test) => {
-      const result = walker.fn(testCase.schema, [])
+      const result = fn(testCase.schema, [])
       test.strictSame(result, testCase.trail)
       test.end()
     })
