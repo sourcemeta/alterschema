0a87fa508c2f79fbc4be1c8c2c99d25d37c8b667
Juan Cruz Viotti <jv@jviotti.com>
2022-05-24 19:58:19 -0400

    Even more simplification
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/walkers/runner.spec.js
+++ test/walkers/runner.spec.js
@@ -1,6 +1,12 @@
 const tap = require('tap')
+const fs = require('fs')
+const path = require('path')
 
-for (const walker of [ 'jsonschema-2019-09' ]) {
+for (const walker of fs.readdirSync(__dirname).filter((name) => {
+  return path.extname(name) === '.json'
+}).map((name) => {
+  return path.basename(name, path.extname(name))
+})) {
   for (const testCase of require(`./${walker}.json`)) {
     const fn = require(`../../lib/walkers/${walker}`)
     tap.test(`${walker.name}: ${testCase.name}`, (test) => {
