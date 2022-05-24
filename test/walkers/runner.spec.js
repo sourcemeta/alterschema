baf1b0ced3b5fdad1f48728585a8cc0998336b87
Juan Cruz Viotti <jv@jviotti.com>
2022-05-24 19:55:43 -0400

    More simplification
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/walkers/runner.spec.js
+++ test/walkers/runner.spec.js
@@ -1,8 +1,8 @@
 const tap = require('tap')
 
 for (const walker of [ 'jsonschema-2019-09' ]) {
-  for (const testCase of require(`./walkers/${walker}.json`)) {
-    const fn = require(`../lib/walkers/${walker}`)
+  for (const testCase of require(`./${walker}.json`)) {
+    const fn = require(`../../lib/walkers/${walker}`)
     tap.test(`${walker.name}: ${testCase.name}`, (test) => {
       const result = fn(testCase.schema, [])
       test.strictSame(result, testCase.trail)
