6d2ed3287fa5eba5eb1baeed237616885bb4c572
Juan Cruz Viotti <jv@jviotti.com>
2022-05-24 21:02:33 -0400

    More simplification
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/walkers.spec.js
+++ test/walkers.spec.js
@@ -1,14 +1,12 @@
 const tap = require('tap')
 const fs = require('fs')
 const path = require('path')
-const walker = require('../../lib/walker')
+const walker = require('../lib/walker')
 
-for (const walkerName of fs.readdirSync(__dirname).filter((name) => {
-  return path.extname(name) === '.json'
-}).map((name) => {
+for (const walkerName of fs.readdirSync(path.resolve(__dirname, 'walkers')).map((name) => {
   return path.basename(name, path.extname(name))
 })) {
-  for (const testCase of require(`./${walkerName}.json`)) {
+  for (const testCase of require(`./walkers/${walkerName}.json`)) {
     tap.test(`${walker.name}: ${testCase.name}`, (test) => {
       const result = walker(walkerName, testCase.schema, [])
       test.strictSame(result, testCase.trail)
