db7637d76befc3a85474b899a217d888e72802f0
Juan Cruz Viotti <jv@jviotti.com>
2022-05-26 21:40:42 -0400

    Add JSON-based test cases
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,21 +1,18 @@
 const tap = require('tap')
 const alterschema = require('..')
 
-tap.test('basic JSON Schema 2019-09 to 2020-12', (test) => {
-  const schema = {
-    type: 'array',
-    items: [
-      { type: 'string' }
-    ]
+for (const suite of [
+  {
+    from: '2019-09',
+    to: '2020-12',
+    tests: require('./jsonschema-2019-19-to-2020-12.json')
   }
-
-  const result = alterschema(schema, '2019-09', '2020-12')
-  test.strictSame(result, {
-    type: 'array',
-    prefixItems: [
-      { type: 'string' }
-    ]
-  })
-
-  test.end()
-})
+]) {
+  for (const testCase of suite.tests) {
+    tap.test(`${suite.from} => ${suite.to}: ${testCase.name}`, (test) => {
+      const result = alterschema(testCase.schema, suite.from, suite.to)
+      test.strictSame(result, testCase.expected)
+      test.end()
+    })
+  }
+}
