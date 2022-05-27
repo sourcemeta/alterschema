192329ed441c552d2495a7d171b2340e74edc020
Juan Cruz Viotti <jv@jviotti.com>
2022-05-27 12:59:31 -0400

    Remove comment
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- rules/jsonschema-2019-09-to-2020-12.js
+++ rules/jsonschema-2019-09-to-2020-12.js
@@ -3,8 +3,6 @@ const jsonschema = require('../lib/jsonschema')
 
 // See https://json-schema.org/draft/2020-12/release-notes.html
 
-      // return jsonschema.usesVocabulary(root, value, 'https://json-schema.org/draft/2019-09/vocab/validation') &&
-
 exports.WALKER = 'jsonschema-2019-09'
 exports.RULES = [
   {
