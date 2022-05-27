aee1fc83097e9b69dadb0352565ee0e78f18c64b
Juan Cruz Viotti <jv@jviotti.com>
2022-05-27 12:50:24 -0400

    Inline rules
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- rules/jsonschema-2019-09-to-2020-12.js
+++ rules/jsonschema-2019-09-to-2020-12.js
@@ -3,22 +3,23 @@ const jsonschema = require('../lib/jsonschema')
 
 // See https://json-schema.org/draft/2020-12/release-notes.html
 
-exports.itemsToPrefixItems = {
-  condition: (value, root) => {
-    return jsonschema.usesVocabulary(root, value, 'https://json-schema.org/draft/2019-09/vocab/validation') &&
-      Array.isArray(value.items)
-  },
-  transform: {
-    $merge: [
-      { $eval: 'omit(schema, "items")' },
-      {
-        prefixItems: {
-          $eval: 'schema.items'
-        }
-      }
-    ]
-  }
-}
-
 exports.WALKER = 'jsonschema-2019-09'
-exports.RULES = [ exports.itemsToPrefixItems ]
+exports.RULES = [
+  {
+    name: 'items to prefix items',
+    condition: (value, root) => {
+      return jsonschema.usesVocabulary(root, value, 'https://json-schema.org/draft/2019-09/vocab/validation') &&
+        Array.isArray(value.items)
+    },
+    transform: {
+      $merge: [
+        { $eval: 'omit(schema, "items")' },
+        {
+          prefixItems: {
+            $eval: 'schema.items'
+          }
+        }
+      ]
+    }
+  }
+]
