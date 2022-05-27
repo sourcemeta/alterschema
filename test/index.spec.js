3f9f8b5c6066abd5f396709f92931f5c2e24915b
Juan Cruz Viotti <jv@jviotti.com>
2022-05-26 21:42:20 -0400

    Single test file
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,7 @@
 const tap = require('tap')
+const fs = require('fs')
+const path = require('path')
+const walker = require('../lib/walker')
 const alterschema = require('..')
 
 for (const suite of [
@@ -12,6 +15,18 @@ for (const suite of [
     tap.test(`${suite.from} => ${suite.to}: ${testCase.name}`, (test) => {
       const result = alterschema(testCase.schema, suite.from, suite.to)
       test.strictSame(result, testCase.expected)
+      test.end()
+    })
+  }
+}
+
+for (const walkerName of fs.readdirSync(path.resolve(__dirname, 'walkers')).map((name) => {
+  return path.basename(name, path.extname(name))
+})) {
+  for (const testCase of require(`./walkers/${walkerName}.json`)) {
+    tap.test(`${walkerName}: ${testCase.name}`, (test) => {
+      const result = walker(walkerName, testCase.schema, [])
+      test.strictSame(result, testCase.trail)
       test.end()
     })
   }
