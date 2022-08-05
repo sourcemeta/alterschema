2f9893ab4afccbcc5b2431e976e9184a9db9407f
Juan Cruz Viotti <jv@jviotti.com>
2022-08-05 15:47:07 -0400

    Handle draft4 "id" when validating JSON Schema definitions (#41)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- bindings/node/jsonschema.js
+++ bindings/node/jsonschema.js
@@ -1,4 +1,6 @@
 const jsonschema = require('@hyperjump/json-schema')
+const METASCHEMAS = require('../../metaschemas.json')
+jsonschema.setMetaOutputFormat(jsonschema.FLAG)
 
 // TODO: This is a mock implementation. Ideally, we look at the metaschema
 exports.usesVocabulary = (_root, value, _vocabulary) => {
@@ -10,7 +12,8 @@ exports.usesVocabulary = (_root, value, _vocabulary) => {
 }
 
 exports.matches = async (schema, value) => {
+  const idProperty = METASCHEMAS[schema.$schema] === 'draft4' ? 'id' : '$id'
   jsonschema.add(schema)
-  const result = await jsonschema.validate(jsonschema.get(schema.$id), value)
+  const result = await jsonschema.validate(jsonschema.get(schema[idProperty]), value)
   return result.valid
 }
