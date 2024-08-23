5a679a06edf3be4fea898a841e1aea67e7cfa63b
Juan Cruz Viotti <jv@jviotti.com>
2024-08-23 13:24:55 -0400

    Port engine component from JSON Toolkit
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/engine/include/sourcemeta/alterschema/engine.h
@@ -0,0 +1,8 @@
+#ifndef SOURCEMETA_ALTERSCHEMA_ENGINE_H_
+#define SOURCEMETA_ALTERSCHEMA_ENGINE_H_
+
+#include <sourcemeta/alterschema/engine_bundle.h>
+#include <sourcemeta/alterschema/engine_rule.h>
+#include <sourcemeta/alterschema/engine_transformer.h>
+
+#endif
